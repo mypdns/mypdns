db3119ac5d60dc0b3857ea5d285ac4b3833c5ec5
spirillen <44526987+spirillen@users.noreply.github.com>
2020-10-25 04:29:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Spirillen",
+    version="0.0.1.dev",
+    author="spirillen",
+    author_email="$PIPI_EMAIL",
+    description="Name space reservation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.mypdns.org/",
+    project_urls={
+        'Tracker': 'https://www.mypdns.org/',
+        'Source': 'https://github.com/pypa/sampleproject/',
+        },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.7',
+)
