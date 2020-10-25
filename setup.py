761f9f744d38397e78104d066feebd629bd559f6
Spirillen <pdns@protonmail.com>
2020-10-25 07:11:44 +0100

    Hmm wondering

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mypdns",
-    version="0.0.1.dev2",
+    version="0.0.1.dev3",
     author="spirillen",
     author_email="pdns@protonmail.com",
     description="Name space reservation",
@@ -18,9 +18,24 @@ setuptools.setup(
         },
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Environment :: Console",
+	"Development Status :: 7 - Inactive",
+	"Intended Audience :: Developers",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.7",
+	"Programming Language :: Python :: 3.8",
+	"Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
         "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.7',
+    keywords=[
+            "dns",
+            "domain",
+            "IP",
+            "IPv4",
+            "IPv6",
+            "URL",
+            "WHOIS",
+        ],
+    python_requires='>=3.7, <4',
 )
