4a073e38f61519a796de74bc427c312be62599a6
spirillen <2-spirillen@noreply.mypdns.org>
2021-06-26 20:51:10 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mypdns",
-    version="0.0.1.dev4",
+    version="1.0.0",
     author="spirillen",
     author_email="pdns@protonmail.com",
     description="Name space reservation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://www.mypdns.org/",
+    url="https://mypdns.org/",
     project_urls={
-        'Tracker': 'https://www.mypdns.org/tag/matrix/',
-        'Source': 'https://github.com/pypa/sampleproject/',
+        'Tracker': 'https://mypdns.org/python/mypdns/-/issues',
+        'Source': 'https://mypdns.org/python/mypdns',
         },
     packages=setuptools.find_packages(),
     classifiers=[
         "Environment :: Console",
 	"Development Status :: 7 - Inactive",
 	"Intended Audience :: Developers",
-	"Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3.7",
 	"Programming Language :: Python :: 3.8",
 	"Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
@@ -37,5 +35,5 @@ setuptools.setup(
             "URL",
             "WHOIS",
         ],
-    python_requires='>=3.7, <4',
+    python_requires='>=3.8.0, <4',
 )
